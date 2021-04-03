b2286d17e31b6911c1d67365f5713900b7cda5f6
AbdulnazifSource <abdulnazifmail@gmail.com>
2021-04-03 16:28:11 +0100

    assignment upload

---
+++ section_2.py
@@ -0,0 +1,23 @@
+#Q1
+print("Nazif", "Computer science", sep=" \n")
+#Q2
+scare = "boo!"
+print(scare)
+#Q3
+firstname = "Nazif"
+lastname = "Abdulkadir"
+fullname = firstname + " " + lastname
+print("Firstname: "+ firstname, "Lastname: " + lastname, "Fullname: " + fullname, sep="\n")
+#Q4
+compute = 12 / 0.5
+print("12 / 0.5 = " + str(compute))
+#Q5
+num_1 = 51
+num_2 = 5
+total = num_1 + num_2
+print("sum of two numbers: " + str(total))
+#Q6
+print("6 / 5 = " + str(6%5))
+#Q7
+x = (2 * 4) * (4 + 2)
+print(x)
